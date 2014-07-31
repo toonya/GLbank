4fca6c49e5c81fc9324a5cf58e94b6c7f858d88a
toonya <msc5762@me.com>
2014-07-31 10:52:15 +0800

    add horizontal-screen case style.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
 
+(function($) {
+	$.fn.sizeCheck = function(options) {
+		return this.each(function(){
+			var data = $(this).data();
+			var options = $.extend({}, options, data);
+			options.$this = $(this);
+			new sizeCheck(options);
+		})
+	}
+
+	var sizeCheck = function(options){
+		this.init(options);
+	}
+
+	sizeCheck.prototype = {
+		init: function(options) {
+			this.options = options;
+			this.bind();
+			this.check();
+		},
+		bind: function() {
+			$( window ).resize( $.proxy(function() {
+				this.check();
+			}, this) )
+		},
+		check: function() {
+			var base = $('body'),
+				w    = base.outerWidth(),
+				h    = base.outerHeight();
+			if( w>h )
+				base.addClass( 'horizontal-screen' );
+			else
+				base.removeClass( 'horizontal-screen' );
+		}
+	}
+
+	$('body.mobile').sizeCheck();
+})(jQuery)
